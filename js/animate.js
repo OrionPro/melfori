edcb29b2747eea89c7625160b3735847f03a7aeb
illya <mini_van@ukr.net>
2017-02-14 13:23:48 +0200

    set animation hover (finish)

--- js/animate.js
+++ js/animate.js
@@ -3,26 +3,22 @@
 
 
 class Animation{
-	//привет Илья
+
 
 	constructor(){
 
 
 		this.tl1 = new TimelineMax();
 		this.tl1.pause();
-
-
-		this.developEffect = new TimelineMax();
 		
 	}
 
-	someAnim(){
+	description(){
 		this.tl1.from('.header_title', 0.7, {
 			y: -100,
 			opacity: 0,
 			ease: Power4.easeOut
 		},'+=1');
-
 	}
 
 
@@ -34,6 +30,42 @@ class Animation{
 
 	hovers(){
 
+		TweenMax.set('.who_are_we_item_text',{scaleX:0});
+		TweenMax.set('.who_are_we_item_img img',{scaleX:1});
+		$('.who_are_we_item').hover(function() {
+
+
+			let image = $(this).find('.who_are_we_item_img img');
+			let text = $(this).find('.who_are_we_item_text');
+
+			let inM =  new TimelineMax();
+
+
+			inM.to(image, 0.3, {
+				scaleX: 0
+			});
+			inM.to(text, 0.3, {
+				scaleX: 1
+			},"-=0.1")
+
+
+		}, function() {
+
+
+			let image = $(this).find('.who_are_we_item_img img');
+			let text = $(this).find('.who_are_we_item_text');
+
+			let outM =  new TimelineMax();
+
+
+			outM.add('lable','+=0.20').to(text, 0.3, {
+				scaleX: 0
+			},"lable");
+			outM.add('lable','+=0.05').to(image, 0.3, {
+				scaleX: 1
+			},"lable");
+
+		})
 	}
 }
 
@@ -47,9 +79,11 @@ $(window).scroll(function () {
 });
 
 $(window).ready(function() {
+	anim.hovers();
+
 
 	if (document.documentElement.clientWidth >= 1200) {
-		anim.someAnim();
+		anim.description();
 		anim.play();
 	}
 
